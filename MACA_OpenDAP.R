2368dd8297a2b1a3bd31bb96f5d18f0861193ec5
acrunyon <amber.c.runyon@gmail.com>
2021-11-04 08:17:20 -0600

    adding spatial stuff

--- MACA_OpenDAP.R
+++ MACA_OpenDAP.R
@@ -70,10 +70,79 @@ c <- data.frame(time,data)
 names(c)[2] <- varName
 c$GCM = GCMs[1]
 c$rcp = sub("\\_.*", "", set[1])
-df = 
+
 ## CLOSE THE FILE
 nc_close(nc)
     }
   }
 }
+
+#### Lat/lon spatial
+# centroids <- st_read("C:/Users/achildress/OneDrive - DOI/Documents/GIS/nps_boundary_centroids/nps_boundary_centroids.shp")
+# centroids <- filter(centroids, UNIT_CODE == site)
+
+boundary <- st_read("C:/Users/achildress/OneDrive - DOI/Documents/GIS/nps_boundary/nps_boundary.shp")
+boundary <- filter(boundary, UNIT_CODE == SiteID)
+
+bb <- st_bbox(boundary)
+
+Lat_index = which(All_lat$nc.dim.lat.vals <= c(bb[4]+0.02) & All_lat$nc.dim.lat.vals >= c(bb[2]-0.02))
+Lon_index = which(All_lon$nc.dim.lon.vals <= c(360 + bb[3]+0.02) & All_lon$nc.dim.lon.vals >= c(360 + bb[1]-0.02))
+
+src <- tidync(url)
+variable_names <- src$variable
+available_times <- src %>% activate("D3") %>% hyper_tibble()
+
+colnames(variable_names) <- "Available variable"
+colnames(available_times) <- "Available times"
+available_times$`Available times` <- as.numeric(available_times$`Available times`)
+available_times$index = 1
+
+Pulled_data <- src %>% 
+  hyper_filter(lat = lat <= c(bb[4]+0.02) & lat >= c(bb[2]-0.02)) %>% 
+  hyper_filter(lon = lon <= c(365 + bb[3]+0.02) & lon >= c(365+ bb[1]-0.02)) %>% 
+  hyper_tibble(select_var = "precipitation")
+
+s<-st_as_stars(Pulled_data,values="precipitation",dimensions=st_dimensions(lat=lat,lon=lon,time=time),crs=4326)
+  
+  st_as_sf(coords = c("lon", "lat"), crs = 4326, agr = "constant")
+
+p <- st_as_stars(Pulled_data)
+
+head(Pulled_data)
+
+check_filter <- Pulled_data %>% filter(time == min(Pulled_data$time))
+
+ggplot() +
+  geom_sf(data = boundaries, fill = "cornflowerblue") +
+  geom_sf(data = check_filter, color = "red", size=0.1) +
+  coord_sf(crs = 4326) 
+
+
+df = data.frame()
+
+# system.time(
+ Pulled_data <- src %>% 
+      hyper_filter(lat = lat <= c(bb[4]+0.02) & lat >= c(bb[2]-0.02)) %>% 
+      hyper_filter(lon = lon <= c(360 + bb[3]+0.02) & lon >= c(360 + bb[1]-0.02))
+ 
+ st_as_stars(Pulled_data)
+ 
+ stars::read_stars(Pulled_data)
+ 
+ 
+ cube <- hyper_tbl_cube(src %>%
+                          activate(precipitation), lon = lon <= c(360 + bb[3]+0.02) & lon >= c(360 + bb[1]-0.02),
+                        lat = lat <= c(bb[4]+0.02) & lat >= c(bb[2]-0.02))
+
+     
+    df1 <- Pulled_data %>% group_by(time) %>% summarise_at(1,mean)
+    df1$GCM = sub("^[^_]*_", "",names(df1[2]))
+    df1$variable = sub("_.*","", names(df1[2]))
+    names(df1)[2] <- "value"
+    df = rbind(df,df1)
+)
+
+
+# data <- ncvar_get(nc, varName, start=c(Lon_index[1],Lat_index[1],1),count=c(length(Lon_index),length(Lat_index),endcount))
 
