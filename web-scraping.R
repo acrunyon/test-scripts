891f0ddb15662490ec1dcc7798b5cb995d50eb71
acrunyon <amber.c.runyon@gmail.com>
2022-02-19 16:53:07 -0700

    Tried several new things including xpath and xml. Both seem like they should work but I realized the site is timing out. Reached out to Jared @ ncei to discuss issue.

--- web-scraping.R
+++ web-scraping.R
@@ -19,11 +19,13 @@ webpage <- read_html(url)
 
 ## Testing
 forecasts <- read_html("https://forecast.weather.gov/MapClick.php?lat=40.58931000000007&lon=-105.09268999999995#.YDkzeGhKhPY") %>%
-   html_nodes(".temp") %>% #figure out what these do!
+   html_nodes(".temp") %>% 
    html_text()
  
 forecasts
 
+
+### reading table
 url<-"https://www.ncdc.noaa.gov/cag/county/time-series/CO-069/tavg/all/12/1895-2020"
 noaa <- read_html(url)
 
@@ -37,8 +39,9 @@ y<- url %>%
 tables <- noaa %>% html_table(fill = TRUE)
 first_table <- tables[[1]]
  
-noaa <- read_html("https://www.ncdc.noaa.gov/cag/county/time-series/CO-069/tavg/all/12/1895-2020") %>%
-  html_nodes("div class") %>%
+
+test <- read_html("https://www.ncdc.noaa.gov/cag/county/time-series/CO-069/tavg/all/12/1895-2020") %>%
+  html_nodes(".value") %>%
   html_text()
 
 head(noaa)
@@ -47,3 +50,34 @@ json<-"https://www.ncdc.noaa.gov/cag/county/time-series/CO-069-tavg-all-12-1895-
 r<-read_html(json) %>% html_text()
 head(r)
 r[[1]]
+
+
+
+# from https://www.r-bloggers.com/2015/01/using-rvest-to-scrape-an-html-table/
+url <- "https://www.ncdc.noaa.gov/cag/county/time-series/CO-069/tavg/all/12/1895-2020"
+test <- url %>%
+  read_html() %>%
+  html_element(xpath='//*[@id="valuesTable"]') %>%
+  html_table()
+test <- test[[1]]
+
+test <- read_html(url)
+html_node(test,"id.valuesTable") %>% 
+  html_table()
+
+library(XML)
+xml <- "https://www.ncdc.noaa.gov/cag/county/time-series/CO-069-tavg-all-12-1895-2020.xml"
+x <- read_html(xml) %>% html_text()
+x
+
+# https://medium.com/geekculture/reading-xml-files-in-r-3122c3a2a8d9
+library(xml2)
+library(curl)
+read_html(curl('xml', handle = curl::new_handle("useragent" = "Mozilla/5.0")))
+
+x <- read_xml(xml)
+x
+
+
+x_xml <- xmlParse(x)
+xmlToDataFrame(nodes=)
