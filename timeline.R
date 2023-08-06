753913d41f7a209b09b2f8a21c70d98b3a3ca3ac
Runyon <arunyon@nps.gov>
2023-08-06 13:45:38 -0600

    Script to create timleine from CSV

---
+++ timeline.R
@@ -0,0 +1,9 @@
+# library(devtools)
+# devtools::install_github("daheelee/timelineS")
+# https://github.com/daheelee/timelineS
+
+library(timelineS)
+timelineS(mj_life, main = "Life of Michael Jackson")
+
+mj_life
+#Events   Event_Dates
