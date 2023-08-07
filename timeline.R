7d120dedea8dad2686e6d3f50e1af24aab9c05f7
Runyon <arunyon@nps.gov>
2023-08-06 18:17:19 -0600

    troubleshoot

--- timeline.R
+++ timeline.R
@@ -3,7 +3,14 @@
 # https://github.com/daheelee/timelineS
 
 library(timelineS)
-timelineS(mj_life, main = "Life of Michael Jackson")
+timelineS(mj_life, main = "Life of Michael Jackson",labels=mj_life$Events)
 
 mj_life
 #Events   Event_Dates
+
+ccsp <- read.csv("C:/Users/arunyon/Downloads/Events.csv",stringsAsFactors = F)
+ccsp$Event_Dates<-as.Date(paste(ccsp$Year,ccsp$Month,"01",sep="-"),format="%Y-%m-%d")
+timelineS(ccsp,main="National Park Service\n Climate Change Scenario Planning",labels=ccsp$Events)
+
+
+
