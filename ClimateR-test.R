17a3abbd0287a0bf3b89c77e8cc996efca8738ac
Runyon <arunyon@nps.gov>
2023-08-08 08:55:00 -0600

    Get working with new updates

--- ClimateR-test.R
+++ ClimateR-test.R
@@ -2,15 +2,20 @@
 # https://github.com/mikejohnson51/climateR
  
 remotes::install_github("mikejohnson51/climateR")
+remotes::install_github("mikejohnson51/AOI")
 library(climateR)
+library(AOI)
+
+
 
 # Needs aoi to run -- dig back through original cft code at how to create aoi from lat/lon
-future = getMACA(geocode("Fort Collins", pt = TRUE), 
-                 model = 5, param = "tmax", 
+AOI<-geocode(location = c("Fort Collins"), pt = TRUE) 
+future = getMACA(AOI, 
+                 model = 5, varname = "tasmax", 
                  startDate = "2050-01-01", endDate = "2050-01-31")
 #> Spherical geometry (s2) switched off
 #> Spherical geometry (s2) switched on
-
+x<-as.data.frame(future)
 future_long = future %>% 
   dplyr::select(-source, -lat, -lon) %>% 
   tidyr::pivot_longer(-date) 
