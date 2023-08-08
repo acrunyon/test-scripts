b9250bdfb6d05e6e305ae781f5956f6173736d0c
Runyon <arunyon@nps.gov>
2023-08-08 08:11:52 -0600

    Change text size of timeline

--- timeline.R
+++ timeline.R
@@ -3,14 +3,15 @@
 # https://github.com/daheelee/timelineS
 
 library(timelineS)
-timelineS(mj_life, main = "Life of Michael Jackson",labels=mj_life$Events)
-
-mj_life
+# timelineS(mj_life, main = "Life of Michael Jackson",labels=mj_life$Events)
+# 
+# mj_life
 #Events   Event_Dates
 
 ccsp <- read.csv("C:/Users/arunyon/Downloads/Events.csv",stringsAsFactors = F)
 ccsp$Event_Dates<-as.Date(paste(ccsp$Year,ccsp$Month,"01",sep="-"),format="%Y-%m-%d")
-timelineS(ccsp,main="National Park Service\n Climate Change Scenario Planning",labels=ccsp$Events)
+timelineS(ccsp,main="National Park Service\n Climate Change Scenario Planning",labels=ccsp$Events,buffer.days=0,
+          scale.font=2,scale.cex=2,label.cex=1.3)
 
 
 
