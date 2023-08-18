0c48714800607744f3aec61c8a38bb7609d98e6a
Runyon <arunyon@nps.gov>
2023-08-18 11:56:05 -0600

    Script looping through variables and stitching into df works. get NA values when doing it at once. Think timing out server?

--- ClimateR-test.R
+++ ClimateR-test.R
@@ -1,24 +1,57 @@
-# Testing ClimateR as temporary subsistute for cft()
-# https://github.com/mikejohnson51/climateR
- 
-remotes::install_github("mikejohnson51/climateR")
-remotes::install_github("mikejohnson51/AOI")
+# # Testing ClimateR as temporary subsistute for cft()
+# # https://github.com/mikejohnson51/climateR
+#  
+# remotes::install_github("mikejohnson51/climateR")
+# remotes::install_github("mikejohnson51/AOI")
 library(climateR)
 library(AOI)
+library(dplyr)
+library(ggplot2)
 
-
+rm(list=ls())
 
 # Needs aoi to run -- dig back through original cft code at how to create aoi from lat/lon
-AOI<-geocode(location = c("Fort Collins"), pt = TRUE) 
-future = getMACA(AOI, 
-                 model = 5, varname = "tasmax", 
+# AOI<-geocode(location = c("Fort Collins"), pt = TRUE) 
+AOI<- aoi_get(list(42.735558, -99.743792,.01,.01)) #Coordiantes of east NIOB location
+
+vars = c("tasmax", "tasmin", "pr", "rhsmax", "rhsmin")
+# scens = c("rcp45", "rcp85")
+
+start.time <-Sys.time()
+future_all <- data.frame()
+for (i in 1:length(vars)){
+future1 = getMACA(AOI, 
+                 model = 2, varname = vars[i], scenario  = "rcp45",
                  startDate = "2050-01-01", endDate = "2050-01-31")
-#> Spherical geometry (s2) switched off
-#> Spherical geometry (s2) switched on
-x<-as.data.frame(future)
-future_long = future %>% 
-  dplyr::select(-source, -lat, -lon) %>% 
-  tidyr::pivot_longer(-date) 
+future2 = getMACA(AOI, 
+                  model = 2, varname = vars[i], scenario  = "rcp85",
+                  startDate = "2050-01-01", endDate = "2050-01-31")
+future<- left_join(future1, future2, by="date")
+
+future_long = future |>  
+  tidyr::pivot_longer(-date)
+FL <- future_long |> 
+  mutate(GCM = gsub("^[^_]*_([^_]+)_.*$", "\\1", future_long$name),
+         RCP = sub('.*_', '', future_long$name)) |> 
+  rename(!!vars[i]:=value) |> select(-c(name))
+if(i==1) { future_all = FL } else {
+  future_all = left_join(future_all, FL, by=c("date","GCM","RCP"))
+  rm(future_long, FL,future1, future2, future)
+  }
+}
+end.time <- Sys.time()
+end.time-start.time
+
+future1 = getMACA(AOI, 
+                  model = 2, varname = vars[4], scenario  = "rcp45",
+                  startDate = "2050-01-01", endDate = "2050-01-31")
+
+
+
+# sub("\\_.*", "", future_long$name) #extracts variable name
+# sub('.*_', '', future_long$name) #extracts rcp
+# gsub("^[^_]*_([^_]+)_.*$", "\\1", future_long$name) #extracts gcm
+
 
 ggplot(data = future_long, aes(x = date, y = value, col = name)) + 
   geom_line() + 
