fd16ab787967911130415482f909c153fce33aaa
Runyon <arunyon@nps.gov>
2023-12-04 09:36:35 -0700

    Update timelines plot

--- timeline.R
+++ timeline.R
@@ -10,8 +10,10 @@ library(timelineS)
 
 ccsp <- read.csv("C:/Users/arunyon/Downloads/Events.csv",stringsAsFactors = F)
 ccsp$Event_Dates<-as.Date(paste(ccsp$Year,ccsp$Month,"01",sep="-"),format="%Y-%m-%d")
-timelineS(ccsp,main="National Park Service\n Climate Change Scenario Planning",labels=ccsp$Events,buffer.days=0,
-          scale.font=2,scale.cex=2,label.cex=1.3)
+timelineS(ccsp,main="National Park Service\n Climate Change Scenario Planning",cex=1.5,labels=ccsp$Events,buffer.days=0,
+          scale.font=2,scale.cex=2,label.cex=1.3,label.direction = "downup",label.angle = 90)
 
+dev.copy(device = png, filename = 'C:/Users/arunyon/Downloads/timeline.png', width = 1500, height = 600); dev.off ()
 
-
+##!! Update using this https://benalexkeen.com/creating-a-timeline-graphic-using-r-and-ggplot2/
+# More customizable
