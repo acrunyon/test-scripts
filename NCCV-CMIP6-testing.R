b8760d77acbd0e28275df3c134e545a01067e9d4
acrunyon <amber.c.runyon@gmail.com>
2025-06-24 15:57:23 -0600

    scatterplots

--- NCCV-CMIP6-testing.R
+++ NCCV-CMIP6-testing.R
@@ -9,7 +9,7 @@ rm(list=ls())
 
 ################
 # extract raw monthly data
-nc = tidync("D:/LOCA2/CMIP6-LOCA2_1950-2100_NPS_monthly.nc")
+nc = tidync("E:/LOCA2/CMIP6-LOCA2_1950-2100_NPS_monthly.nc")
 
 projections = nc %>% activate("ensemble") %>% hyper_tibble() # extracts values of all projections
 parks = nc %>% activate("UNIT_CODE") %>% hyper_tibble()
@@ -65,32 +65,158 @@ plot(park[1], add=TRUE, col="transparent",border="black",lwd=4)
 
 
 
+
+
 # Scatterplots
 
 # read csv and subset to ACAD, BAND, CRLA, CHAT, DETO
-cmip6 = read.csv("E:/LOCA2/LOCA2-monthly-all-units.csv")
+# cmip6 = read.csv("E:/LOCA2/LOCA2-monthly-all-units.csv")
 units = c("ACAD", "BAND", "CRLA", "CHAT", "DETO")
+# 
+# c6 = cmip6 %>% filter(SiteID %in% units)
 
-c6 = cmip6 %>% filter(SiteID %in% units)
+cmip6 = read.csv("E:/LOCA2/LOCA2-test-set.csv")
 
-c = c6 %>% mutate(date = as.Date(date,format="%Y-%m-%d"),
+cmip6 = cmip6 %>% mutate(date = as.Date(date,format="%Y-%m-%d"),
                   year = format(date,"%Y"),
-                  period = ifelse(year < 2015, "Historical", "Future"))
+                  period = ifelse(year < 2015, "Historical", "Future"),
+                  pr_in = pr / 25.4 * 30, # mm to inches and multiply by 30 days in month
+                  tmean_f = (tas * 9/5) + 32) # Celsius to Fahrenheit
 
 
-hist = c %>% filter(year> 1980 & year < 2011) %>% 
-  select(SiteID, GCM, date, year, pr, tas, period) %>% 
-  group_by(year, SiteID, GCM) %>% mutate(pr_sum = sum(pr)) %>% 
-  group_by(SiteID, GCM) %>% summarise(tmean = mean(tas),
-                                      precip = mean(pr_sum))
+hist = cmip6 %>% filter(year> 1980 & year < 2011) %>% 
+  select(SiteID, GCM, date, year, pr_in, tmean_f, period) %>% 
+  group_by(year, SiteID, GCM) %>% mutate(pr_in_sum = sum(pr_in)) %>% 
+  group_by(SiteID, GCM) %>% summarise(tmean = mean(tmean_f),
+                                      precip = mean(pr_in_sum))
 
-fut = c %>% filter(year> 2034 & year < 2066) %>% 
-  select(SiteID, GCM, date, year, pr, tas, period) %>% 
-  group_by(year, SiteID, GCM) %>% mutate(pr_sum = sum(pr)) %>% 
-  group_by(SiteID, GCM) %>% summarise(tmean = mean(tas),
-                                      precip = mean(pr_sum))
+fut = cmip6 %>% filter(year> 2034 & year < 2066) %>% 
+  select(SiteID, GCM, date, year, pr_in, tmean_f, period) %>% 
+  group_by(year, SiteID, GCM) %>% mutate(pr_in_sum = sum(pr_in)) %>% 
+  group_by(SiteID, GCM) %>% summarise(tmean = mean(tmean_f),
+                                      precip = mean(pr_in_sum))
 
 
+fut_delta = fut %>% left_join(hist, by = c("SiteID", "GCM")) %>% 
+  mutate(tmean_delta = tmean.x - tmean.y,
+         precip_delta = precip.x - precip.y) %>% 
+  select(c(SiteID, GCM, tmean_delta, precip_delta)) %>% 
+  mutate(ssp = stringr::str_sub(GCM, -6, -1))
+
+
+# No color
+library(ggplot2)
+library(ggrepel)
+
+site = units[5]
+
+loca_df = fut_delta %>% filter(SiteID == site)
+
+cmip5_park = read.csv(paste0("E:/RCF_2024/RCF_opened/", site, "/input-data/", site, "_Future_Means.csv"))
+
+tmean_range = c(min(loca_df$tmean_delta, cmip5_park$DeltaTavg), max(loca_df$tmean_delta, cmip5_park$DeltaTavg))
+precip_range = c(min(loca_df$precip_delta, cmip5_park$DeltaPr*365), max(loca_df$precip_delta, cmip5_park$DeltaPr*365))
+
+
+
+local_df %>% ggplot(aes(tmean_delta, precip_delta, xmin = quantile(tmean_delta, 0.25),
+             xmax = quantile(tmean_delta, 0.75),
+             ymin = quantile(precip_delta, 0.25),
+             ymax = quantile(precip_delta, 0.75))) +
+  geom_text_repel(aes(label=GCM)) +
+  geom_point(colour="black",size=4) +
+  geom_point(aes(color=ssp),size=4) + 
+  theme(axis.text=element_text(size=16),
+        axis.title.x=element_text(size=16,vjust=-0.2),
+        axis.title.y=element_text(size=16,vjust=0.2),
+        plot.title=element_text(size=20,face="bold",vjust=2,hjust=0.5),
+        legend.text=element_text(size=16), legend.title=element_text(size=16)) + 
+  theme_classic() +
+  ###
+  labs(title =paste(site," Changes in climate means in 2050 by GCM run (CMIP6)",sep=""), 
+       x = "Changes in average temp", # Change
+       y = "Changes in precip") + #change
+  scale_color_manual(name="Scenarios", values=c("blue", "gold", "red")) +
+  # scale_fill_manual(name="Scenarios",values = c("black")) + 
+  geom_rect(color = "black", alpha=0) +
+  geom_hline(aes(yintercept=mean(precip_delta)),linetype=2) + #change
+  geom_vline(aes(xintercept=mean(tmean_delta)),linetype=2) #change
+
+
+ggsave(paste0(site, "_CMIP6_ssps.jpg"), path = "C:/Users/arunyon/OneDrive - DOI/Documents/CFs/CMIP6-transition/", height=9, width=12,bg="white")
+
+
+
+loca_plot = loca_df %>% ggplot(aes(tmean_delta, precip_delta, xmin = quantile(tmean_delta, 0.25),
+                                      xmax = quantile(tmean_delta, 0.75),
+                                      ymin = quantile(precip_delta, 0.25),
+                                      ymax = quantile(precip_delta, 0.75))) +
+geom_text_repel(aes(label=GCM)) +
+  geom_point(colour="black",size=4) +
+  theme(axis.text=element_text(size=16),
+        axis.title.x=element_text(size=16,vjust=-0.2),
+        axis.title.y=element_text(size=16,vjust=0.2),
+        plot.title=element_text(size=20,face="bold",vjust=2,hjust=0.5),
+        legend.text=element_text(size=16), legend.title=element_text(size=16)) + 
+  theme_classic() +
+  ###
+  labs(title =paste(site," Changes in climate means in 2050 by GCM run (CMIP6)",sep=""), 
+       x = "Changes in average temp", # Change
+       y = "Changes in precip") + #change
+  scale_color_manual(name="Scenarios", values=c("black")) +
+  # scale_fill_manual(name="Scenarios",values = c("black")) + 
+  theme(legend.position="none") +
+  geom_rect(color = "black", alpha=0) +
+geom_hline(aes(yintercept=mean(precip_delta)),linetype=2) + #change
+  geom_vline(aes(xintercept=mean(tmean_delta)),linetype=2) + #change
+  xlim(tmean_range[1], tmean_range[2]) +
+  ylim(precip_range[1], precip_range[2])
+
+
+# No color
+maca_plot = ggplot(cmip5_park, aes(DeltaTavg, DeltaPr*365, xmin=quantile(DeltaTavg, 0.25), 
+                       xmax = quantile(DeltaTavg, 0.75),
+                       ymin = quantile(DeltaPr*365, 0.25),
+                       ymax = quantile(DeltaPr*365, 0.75))) +
+geom_text_repel(aes(label=GCM)) +
+  geom_point(colour="black",size=4) +
+  theme(axis.text=element_text(size=16),
+        axis.title.x=element_text(size=16,vjust=-0.2),
+        axis.title.y=element_text(size=16,vjust=0.2),
+        plot.title=element_text(size=20,face="bold",vjust=2,hjust=0.5),
+        legend.text=element_text(size=16), legend.title=element_text(size=16)) + 
+  theme_classic() +
+  ###
+  labs(title =paste(site," Changes in climate means in 2050 by GCM run (CMIP5)",sep=""), 
+       x = "Changes in average temp", # Change
+       y = "Changes in precip") + #change
+  scale_color_manual(name="Scenarios", values=c("black")) +
+  # scale_fill_manual(name="Scenarios",values = c("black")) + 
+  theme(legend.position="none") +
+  geom_rect(color = "black", alpha=0) + 
+  geom_hline(aes(yintercept=mean(DeltaPr*365)),linetype=2) + #change
+  geom_vline(aes(xintercept=mean(DeltaTavg)),linetype=2) + #change
+  xlim(tmean_range[1], tmean_range[2]) +
+  ylim(precip_range[1], precip_range[2])
+
+
+g = gridExtra::grid.arrange(maca_plot, loca_plot,nrow=2)
+
+ggsave(paste0(site, "_CMIP5_vs_CMIP6_scatterplot.jpg"), path = "C:/Users/arunyon/OneDrive - DOI/Documents/CFs/CMIP6-transition/", plot = g, height=9, width=12,bg="white")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# PLAY WITH RAW LOCA2 DATA
 
 precip_nc = tidync("E:/LOCA2/raw/pr.ACCESS-CM2.ssp245.r1i1p1f1.2075-2100.LOCA_16thdeg_v20240915.yearly.nc")
 
